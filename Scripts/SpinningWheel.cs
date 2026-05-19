bf843370de347d4cdd65285f640d635e1ab92abe
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-05-19 13:07:06 -0700

    Added func to get color the wheel landed on.

--- Scripts/SpinningWheel.cs
+++ Scripts/SpinningWheel.cs
@@ -3,103 +3,114 @@ using Godot;
 using System.Collections.Generic;
 
 public partial class SpinningWheel : Node2D {
-    [Export]
-    private Texture2D _blackWheel;
+	[Export]
+	private Texture2D _blackWheel;
 
-    [Export]
-    private Texture2D _blueWheel;
+	[Export]
+	private Texture2D _blueWheel;
 
-    [Export]
-    private Texture2D _greenWheel;
+	[Export]
+	private Texture2D _greenWheel;
 
-    [Export]
-    private Texture2D _redWheel;
+	[Export]
+	private Texture2D _redWheel;
 
-    [Export]
-    private Texture2D _yellowWheel;
+	[Export]
+	private Texture2D _yellowWheel;
 
-    private Node2D _wheelParent;
+	private Node2D _wheelParent;
 
-    private Vector2I _wheelSize = new(300, 300);
+	private Vector2I _wheelSize = new(300, 300);
 
-    private List<TextureProgressBar> _bars = [];
-    private RandomNumberGenerator _rng = new();
-    private bool _isSpinning = false;
-    private double _durationInSeconds = 4;
-    private double _secondsElapsed;
-    private float _targetRotation;
-    private List<WheelSizeDto> _wheelSizes;
+	private List<TextureProgressBar> _bars = [];
+	private RandomNumberGenerator _rng = new();
+	private bool _isSpinning = false;
+	private double _durationInSeconds = 4;
+	private double _secondsElapsed;
+	private float _targetRotation;
+	private List<ColorDto> _colorDtos; // Offset must be in ascending order
 
-    public override void _Ready() {
-        _InitWheel([
-            new WheelSizeDto(18.42f, 0f, _blackWheel, "Black"),
-            new WheelSizeDto(27.15f, 18.42f, _blueWheel, "Blue"),
-            new WheelSizeDto(11.73f, 45.57f, _greenWheel, "Green"),
-            new WheelSizeDto(29.88f, 57.30f, _redWheel, "Red"),
-            new WheelSizeDto(12.82f, 87.18f, _yellowWheel, "Yellow")
-        ]);
-        _rng.Randomize();
-        _StartSpin();
-    }
+	public override void _Ready() {
+		_InitWheel([
+			new ColorDto(18.42f, 0f, _blackWheel, "Black"),
+			new ColorDto(27.15f, 18.42f, _blueWheel, "Blue"),
+			new ColorDto(11.73f, 45.57f, _greenWheel, "Green"),
+			new ColorDto(29.88f, 57.30f, _redWheel, "Red"),
+			new ColorDto(12.82f, 87.18f, _yellowWheel, "Yellow")
+		]);
+		_rng.Randomize();
+		_StartSpin();
+	}
 
-    public override void _Process(double delta) {
-        if (!_isSpinning) {
-            return;
-        }
+	public override void _Process(double delta) {
+		if (!_isSpinning) {
+			return;
+		}
 
-        _secondsElapsed += delta;
+		_secondsElapsed += delta;
 
-        if (_secondsElapsed >= _durationInSeconds) {
-            float snapRotation = Mathf.DegToRad(_targetRotation % 360);
-            foreach (TextureProgressBar bar in _bars) {
-                bar.Rotation = snapRotation;
-            }
+		if (_secondsElapsed >= _durationInSeconds) {
+			float snapRotation = Mathf.DegToRad(_targetRotation % 360);
+			foreach (TextureProgressBar bar in _bars) {
+				bar.Rotation = snapRotation;
+			}
 
-            float finalAngleDeg = Mathf.RadToDeg(_bars[0].Rotation) % 360;
-            GD.Print($"Final angle: {finalAngleDeg}° | As percentage: {((finalAngleDeg) % 360) / 3.6f:F1}%");
+			float finalAngleDeg = Mathf.RadToDeg(_bars[0].Rotation) % 360;
+			float percentage = 100 - ((finalAngleDeg) % 360) / 3.6f;
+			GD.Print($"Final angle: {finalAngleDeg}° | As percentage: {percentage:F2}% Color: {_GetColor(percentage, _colorDtos)}");
 
-            _isSpinning = false;
-            return;
-        }
+			_isSpinning = false;
+			return;
+		}
 
-        double progress = _secondsElapsed / _durationInSeconds;
-        double eased = 1 - Math.Pow(1 - progress, 3); // ease-out cubic
-        float currentAngle = (float)(_targetRotation * eased);
-        foreach (TextureProgressBar bar in _bars) {
-            bar.Rotation = Mathf.DegToRad(currentAngle);
-        }
-    }
+		double progress = _secondsElapsed / _durationInSeconds;
+		double eased = 1 - Math.Pow(1 - progress, 3); // ease-out cubic
+		float currentAngle = (float)(_targetRotation * eased);
+		foreach (TextureProgressBar bar in _bars) {
+			bar.Rotation = Mathf.DegToRad(currentAngle);
+		}
+	}
 
-    private void _InitWheel(List<WheelSizeDto> wheelSizes) {
-        _wheelSizes = wheelSizes;
-        _wheelParent = new Sprite2D();
-        AddChild(_wheelParent);
+	private void _InitWheel(List<ColorDto> wheelSizes) {
+		_colorDtos = wheelSizes;
+		_wheelParent = new Sprite2D();
+		AddChild(_wheelParent);
 
-        foreach (WheelSizeDto wheelSize in wheelSizes) {
-            TextureProgressBar bar = new TextureProgressBar();
-            _wheelParent.AddChild(bar);
+		foreach (ColorDto wheelSize in wheelSizes) {
+			TextureProgressBar bar = new TextureProgressBar();
+			_wheelParent.AddChild(bar);
 
-            bar.TextureProgress = wheelSize.Texture;
-            bar.FillMode = (int)TextureProgressBar.FillModeEnum.Clockwise;
-            bar.SetRadialInitialAngle(wheelSize.Offset * 3.6f);
-            bar.SetFillDegrees(360f);
-            bar.Value = wheelSize.Percentage + 1;
-            bar.PivotOffset = _wheelSize / 2;
+			bar.TextureProgress = wheelSize.Texture;
+			bar.FillMode = (int)TextureProgressBar.FillModeEnum.Clockwise;
+			bar.SetRadialInitialAngle(wheelSize.Offset * 3.6f);
+			bar.SetFillDegrees(360f);
+			bar.Value = wheelSize.Percentage + 1;
+			bar.PivotOffset = _wheelSize / 2;
 
-            _bars.Add(bar);
-        }
-    }
+			_bars.Add(bar);
+		}
+	}
 
-    private void _StartSpin() {
-        if (_isSpinning) {
-            return;
-        }
+	private ColorDto _GetColor(float percentage, List<ColorDto> colors) {
+		for (int i = 1; i < _colorDtos.Count; i++) {
+			if (colors[i].Offset > percentage) {
+				return colors[i - 1];
+			}
+		}
 
-        int spins = 5;
-        _targetRotation = 360 * spins + _rng.RandfRange(0f, 360f);
-        _secondsElapsed = 0;
-        _isSpinning = true;
-    }
+		return colors[^1];
+	}
 
-    private record WheelSizeDto(float Percentage, float Offset, Texture2D Texture, string Description);
+	private void _StartSpin() {
+		if (_isSpinning) {
+			return;
+		}
+
+		int spins = 5;
+		_targetRotation = 360 * spins + _rng.RandfRange(0f, 360f);
+		_secondsElapsed = 0;
+		_isSpinning = true;
+	}
+
+	private record ColorDto(float Percentage, float Offset, Texture2D Texture, string Description);
 }
